49126a14ad42d7337be9794f0ba7b23b580aad24
koji-dayo <67395053+koji-dayo@users.noreply.github.com>
2021-10-13 14:27:33 +0900

    Commit

--- 7class/ViT.py
+++ 7class/ViT.py
@@ -38,7 +38,8 @@ def train():
   history = model.fit(x_train, y_train,
     batch_size=128,
     epochs=20,
-    validation_data=(x_val,y_val)
+    validation_data=(x_val,y_val),
+    callbacks=callbacks
     )
   score = model.evaluate(x_test, y_test, verbose=0,batch_size=128)
   res = pd.DataFrame(score)
